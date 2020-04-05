7ee6f2aa0c4b4c21096368a03f351e3c3f856c6e
Callum Booth <callum.booth@launchb.co.uk>
2020-04-05 18:43:48 +0100

    gets video working

--- index.js
+++ index.js
@@ -1,119 +1,134 @@
-const dgram = require("dgram");
-const wait = require("waait");
-const app = require("express")();
-const http = require("http").Server(app);
-const io = require("socket.io")(http);
-const throttle = require("lodash/throttle");
-const ffmpeg = require('fluent-ffmpeg');
-const fs = require('fs');
-const {spawn} = require('child_process');
+const dgram = require('dgram')
+const express = require('express')
+const http = require('http')
+const socket = require('socket.io')
+// WebSocket for broadcasting stream to connected clients
+const WebSocket = require('ws')
+const app = express()
+const httpServer = http.Server(app)
+const io = socket(httpServer)
 
-const commandDelays = require("./commandDelays");
+const throttle = require('lodash/throttle')
 
-let h264encoder = false;
+// We'll spawn ffmpeg as a separate process
+const spawn = require('child_process').spawn
 
-const PORT = 8889;
-const HOST = "192.168.10.1";
-const drone = dgram.createSocket("udp4");
-drone.bind(PORT);
+const PORT = 8889
+const HOST = '192.168.10.1'
+const drone = dgram.createSocket('udp4')
+drone.bind(PORT)
 
 function parseState(state) {
-    return state
-        .split(";")
-        .map((x) => x.split(":"))
-        .reduce((data, [key, value]) => {
-            data[key] = value;
-            return data;
-        }, {});
+  return state
+    .split(';')
+    .map((x) => x.split(':'))
+    .reduce((data, [key, value]) => {
+      data[key] = value
+      return data
+    }, {})
 }
 
-const droneState = dgram.createSocket("udp4");
-droneState.bind(8890);
+const droneState = dgram.createSocket('udp4')
+droneState.bind(8890)
 
-const droneStream = dgram.createSocket("udp4");
-droneStream.bind(11111);
-
-drone.on("message", (message) => {
-    console.log(`drone : ${message}`);
-    io.sockets.emit("status", message.toString());
-});
-
-drone.on("error", (err) => {
-    console.log(`drone error: ${err}`);
+drone.on('message', (message) => {
+  console.log('drone:', message.toString())
+  io.sockets.emit('status', message.toString())
 })
 
-droneState.on(
-    "message",
-    throttle((state) => {
-        const formattedState = parseState(state.toString());
-        io.sockets.emit("dronestate", formattedState);
-    }, 1000)
-);
+drone.on('error', (err) => {
+  console.log(`drone error: ${err}`)
+})
 
-droneStream.on("message", message => {
-
-    if (h264encoder !== false) {
-        h264encoder.stdin.write(message.slice(2));
-    }
-});
-
-droneStream.on("listening", () => {
-    var addr = droneStream.address();
-    console.log(`Video listening ${addr.address}:${addr.port}`);
-});
+// droneState.on(
+//   'message',
+//   throttle((state) => {
+//     const formattedState = parseState(state.toString())
+//     io.sockets.emit('dronestate', formattedState)
+//   }, 16)
+// )
 
 function handleError(err) {
-    if (err) {
-        console.log("ERROR");
-        console.log(err);
-    }
-}
-
-function getH264Stream() {
-    const command = 'ffmpeg';
-    const args = [ '-fflags', 'nobuffer', '-f', 'h264', '-i', "-", '-r', '30', '-c:v', 'libx264', '-b:v', '2M', '-profile', 'baseline', '-preset', 'ultrafast', '-tune', 'zerolatency', '-vsync', '0', '-async', '0', '-bsf:v', 'h264_mp4toannexb', '-x264-params','keyint=5:scenecut=0', '-an', '-f', 'h264', '-'];
-    h264encoder = spawn(command, args);
-    
-    let h264chunks = [];
-
-    h264encoder.stderr.on('data', (data) => {
-        //console.log("ffmpeg error", data.toString());
-    });
-    console.log(h264encoder.stdout);
-    h264encoder.stdout.on('data', (data) => {
-        console.log(data) 
-        let idx = data.indexOf(Buffer([0,0,0,1]));
-        if (idx > -1 && h264chunks.length>0) {
-            h264chunks.push(data.slice(0,idx));
-            try {
-                io.sockets.emit("dronestream", Buffer.concat(h264chunks).toString('binary'));
-            } catch (e) { console.log(e) }
-            h264chunks = [];
-            h264chunks.push(data.slice(idx));
-        } else {
-            h264chunks.push(data);
-        }
-    });
-    
+  if (err) {
+    console.log('ERROR')
+    console.log(err)
+  }
 }
 
 //This puts the drone into SDK mode
-drone.send("command", 0, "command".length, PORT, HOST, handleError);
+drone.send('command', 0, 'command'.length, PORT, HOST, handleError)
 
-io.on("connection", (socket) => {
-    socket.on("command", (command) => {
-        console.log(command + " command sent from browser");
-        
-        if (command === "streamon") {
-            getH264Stream();
-        }
-        
-        drone.send(command, 0, command.length, PORT, HOST, handleError);
-    });
-    socket.emit("status", "Connected");
-    io.sockets.emit('dronestream', 'streaming');
-});
+io.on('connection', (socket) => {
+  socket.on('command', (command) => {
+    console.log(command + ' command sent from browser')
 
-http.listen(6767, () => {
-    console.log("Socket io server running on port 6767");
-});
+    drone.send(command, 0, command.length, PORT, HOST, handleError)
+  })
+  socket.emit('status', 'Connected')
+})
+
+httpServer.listen(6767, () => {
+  console.log('Socket io server running on port 6767')
+})
+
+const streamServer = http
+  .createServer(function (request, response) {
+    // Log that a stream connection has come through
+    console.log(
+      'Stream Connection on ' +
+        6768 +
+        ' from: ' +
+        request.socket.remoteAddress +
+        ':' +
+        request.socket.remotePort
+    )
+
+    // When data comes from the stream (FFmpeg) we'll pass this to the web socket
+    request.on('data', function (data) {
+      // Now that we have data let's pass it to the web socket server
+      webSocketServer.broadcast(data)
+    })
+  })
+  .listen(6768) // Listen for streams on port 3001
+
+/*
+  3. Begin web socket server
+*/
+const webSocketServer = new WebSocket.Server({
+  server: streamServer,
+})
+
+// Broadcast the stream via websocket to connected clients
+webSocketServer.broadcast = function (data) {
+  webSocketServer.clients.forEach(function each(client) {
+    if (client.readyState === WebSocket.OPEN) {
+      client.send(data)
+    }
+  })
+}
+// Delay for 3 seconds before we start ffmpeg
+setTimeout(function () {
+  var args = [
+    '-i',
+    'udp://0.0.0.0:11111',
+    '-r',
+    '30',
+    '-s',
+    '960x720',
+    '-codec:v',
+    'mpeg1video',
+    '-b',
+    '800k',
+    '-f',
+    'mpegts',
+    'http://127.0.0.1:6768/stream',
+  ]
+
+  // Spawn an ffmpeg instance
+  var streamer = spawn('ffmpeg', args)
+  // Uncomment if you want to see ffmpeg stream info
+  //streamer.stderr.pipe(process.stderr);
+  streamer.on('exit', function (code) {
+    console.log('Failure', code)
+  })
+}, 3000)
